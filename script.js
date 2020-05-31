fb60d0237c6f73143f359a8318f82cd140efee64
Ajay Vardhan Reddy <47106551+Ajayvardhanreddy@users.noreply.github.com>
2020-05-31 11:21:34 +0530

    update 1.1

--- script.js
+++ script.js
@@ -16,6 +16,11 @@ window.onload = function(){
        choice_number = Math.floor(choice_number);
        return list_choices[choice_number];
     }
+    function win_music() {
+        var sound = new Audio("win.mp3");
+        sound.play();
+        
+      }
     function startgame(user_input){
         comp_choice=bot_choice();
         user_choice=user_input;
@@ -30,25 +35,29 @@ window.onload = function(){
         }
         if(user_choice=="rock" && comp_choice=="scissor"){
             /* user win*/
+            var sound = new Audio("win.mp3");
+            sound.play();
             user_id.src="rock-user.png";
             computer_id.src="scissor-comp.png"; 
             user_count++;
             user_score.value=user_count;
-            document.getElementById("win_value").innerHTML = "Hurray! You won the Match.😀👑"
+            document.getElementById("win_value").innerHTML = "Hurray! You won the Match.😀"
         }
         if(user_choice=="rock" && comp_choice=="rock"){
             /* tie */ 
             user_id.src="rock-user.png";
             computer_id.src="rock-comp.png";
-            document.getElementById("win_value").innerHTML = "Draw Match. Well done Guys!😁🤪"
+            document.getElementById("win_value").innerHTML = "Draw Match. Well done Guys!😁"
         }
         if(user_choice=="paper" && comp_choice=="rock"){
             /* user win*/ 
+            var sound = new Audio("win.mp3");
+            sound.play();
             user_id.src="paper-user.png";
             computer_id.src="rock-comp.png"; 
             user_count++;
             user_score.value=user_count;
-            document.getElementById("win_value").innerHTML = "Hurray! You won the Match.😀👑"
+            document.getElementById("win_value").innerHTML = "Hurray! You won the Match.😀"
         }
         if(user_choice=="paper" && comp_choice=="scissor"){
             /* computer win*/
@@ -62,7 +71,7 @@ window.onload = function(){
             /* tie*/ 
             user_id.src="paper-user.png";
             computer_id.src="paper-comp.png"; 
-            document.getElementById("win_value").innerHTML = "Draw Match. Well done Guys!😁🤪"
+            document.getElementById("win_value").innerHTML = "Draw Match. Well done Guys!😁"
         }
         if(user_choice=="scissor" && comp_choice=="rock"){
             /* comp win*/
@@ -76,15 +85,17 @@ window.onload = function(){
             /*  tie*/ 
             user_id.src="scissor-user.png";
             computer_id.src="scissor-comp.png"; 
-            document.getElementById("win_value").innerHTML = "Draw Match. Well done Guys!😁🤪"
+            document.getElementById("win_value").innerHTML = "Draw Match. Well done Guys!😁"
         }
         if(user_choice=="scissor" && comp_choice=="paper"){
             /* user win*/ 
+            var sound = new Audio("win.mp3");
+            sound.play();
             user_id.src="scissor-user.png";
             computer_id.src="paper-comp.png"; 
             user_count++;
             user_score.value=user_count;
-            document.getElementById("win_value").innerHTML = "Hurray! You won the Match.😀👑"
+            document.getElementById("win_value").innerHTML = "Hurray! You won the Match.😀"
         }
    
 
